a83d2d43449c37e2fdd68bae44ec3ec32bd9f356
Robin Tram <robin.tram@gmail.com>
2018-10-19 01:20:48 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,5 +1,27 @@
 CREATE TABLE characters (
-  
+  id INTEGER PRIMARY KEY,
+    name TEXT,
+    motto TEXT,
+    species TEXT,
+    author_id INTEGER,
+    series TEXT
 );
 
-CREATE TABLE 
+CREATE TABLE books (
+  id INTEGER PRIMARY KEY,
+    title TEXT,
+    year INTEGER,
+    series TEXT
+);
+
+CREATE TABLE series (
+
+);
+
+CREATE TABLE authors (
+
+);
+
+CREATE TABLE sub-genres (
+
+);
