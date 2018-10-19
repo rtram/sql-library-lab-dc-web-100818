004b7ef56a72ce2bc5050e7832c5bd5187bf27af
Robin Tram <robin.tram@gmail.com>
2018-10-19 01:58:20 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,8 +1,8 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT titles,years
-    FROM 
-    
-  "
+    FROM books 
+    ORDER BY
+  ;"
 end
 
 def select_name_and_motto_of_char_with_longest_motto
