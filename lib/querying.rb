157604ca7fb5f89eb95ed48725dd0b78d5ffe9db
Robin Tram <robin.tram@gmail.com>
2018-10-19 02:48:15 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -53,6 +53,7 @@ def select_character_names_and_number_of_books_they_are_in
     JOIN character_books
       ON character_books.character_id = characters.id
     JOIN books 
-      ON character_books.book_id = 
+      ON character_books.book_id = books.id
+      
   ;"
 end
