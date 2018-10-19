c490b5ba33d2048b28f12049c5821d0a0cd68737
Robin Tram <robin.tram@gmail.com>
2018-10-19 02:40:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -41,6 +41,7 @@ def select_series_title_with_most_human_characters
     FROM series
     JOIN characters
       ON series.id = characters.series_id
+    WHERE species = 'human'
     GROUP BY species
  
 
