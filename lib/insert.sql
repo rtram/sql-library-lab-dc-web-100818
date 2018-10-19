9c2f4c0344e44105a2d85764662c1c03f6008740
Robin Tram <robin.tram@gmail.com>
2018-10-19 01:33:49 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,11 +8,11 @@ INSERT INTO subgenres (name) VALUES ("space opera");
 INSERT INTO authors (name) VALUES ("George R. R. Martin");
 INSERT INTO authors (name) VALUES ("Second Author");
 
--- INSERT INTO books (title, year, series_id) VALUES ("Game of Thrones", 1996, 1);
--- INSERT INTO books (title, year, series_id) VALUES ("A Clash of Kings", 1998, 1); 
--- INSERT INTO books (title, year, series_id) VALUES ("A Storm of Swords", 2000, 1); 
--- INSERT INTO books (title, year, series_id) VALUES ("First Book", 2002, 2); 
--- INSERT INTO books (title, year, series_id) VALUES ("Second Book", 2003, 2), (6, "Third Book", 2005, 2);
+INSERT INTO books (title, year, series_id) VALUES ("Game of Thrones", 1996, 1);
+INSERT INTO books (title, year, series_id) VALUES ("A Clash of Kings", 1998, 1); 
+INSERT INTO books (title, year, series_id) VALUES ("A Storm of Swords", 2000, 1); 
+INSERT INTO books (title, year, series_id) VALUES ("First Book", 2002, 2); 
+INSERT INTO books (title, year, series_id) VALUES ("Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
 -- -- INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
 
