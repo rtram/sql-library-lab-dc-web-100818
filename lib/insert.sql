c8dab52061e0c2d95d2b8bc81ed15af77429a98d
Robin Tram <robin.tram@gmail.com>
2018-10-19 01:33:05 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,22 +2,22 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES ("A Song of Ice and Fi
 
 INSERT INTO series (title, author_id, subgenre_id) VALUES ("Second Series", 2, 2);
 
-INSERT INTO subgenres (name) VALUES ("medieval");
-INSERT INTO subgenres (name) VALUES ("space opera");
+-- INSERT INTO subgenres (name) VALUES ("medieval");
+-- INSERT INTO subgenres (name) VALUES ("space opera");
 
-INSERT INTO authors (name) VALUES ("George R. R. Martin");
-INSERT INTO authors (name) VALUES ("Second Author");
+-- INSERT INTO authors (name) VALUES ("George R. R. Martin");
+-- INSERT INTO authors (name) VALUES ("Second Author");
 
-INSERT INTO books (title, year, series_id) VALUES ("Game of Thrones", 1996, 1);
-INSERT INTO books (title, year, series_id) VALUES ("A Clash of Kings", 1998, 1); 
-INSERT INTO books (title, year, series_id) VALUES ("A Storm of Swords", 2000, 1); 
-INSERT INTO books (title, year, series_id) VALUES ("First Book", 2002, 2); 
-INSERT INTO books (title, year, series_id) VALUES ("Second Book", 2003, 2), (6, "Third Book", 2005, 2);
+-- INSERT INTO books (title, year, series_id) VALUES ("Game of Thrones", 1996, 1);
+-- INSERT INTO books (title, year, series_id) VALUES ("A Clash of Kings", 1998, 1); 
+-- INSERT INTO books (title, year, series_id) VALUES ("A Storm of Swords", 2000, 1); 
+-- INSERT INTO books (title, year, series_id) VALUES ("First Book", 2002, 2); 
+-- INSERT INTO books (title, year, series_id) VALUES ("Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
--- INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
+-- -- INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
 
--- INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
+-- -- INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Character One", "motto one", "cylon", 2, 2), (6, "Character Two", "motto two", "human", 2, 2), (7, "Character Three", "motto three", "cylon", 2, 2), (8, "Character Four", "motto four", "cylon", 2, 2);
 
--- INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+-- -- INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
--- INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
+-- -- INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
