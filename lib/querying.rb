8602d1a8f178327a02c8cadded0a83d46db38b6b
Robin Tram <robin.tram@gmail.com>
2018-10-19 02:25:24 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name, series.subgenre
+  "SELECT authors.name, series.
   ;"
 end
 
