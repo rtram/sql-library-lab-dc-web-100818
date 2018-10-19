cb940aaafc1bf29d2da683bd7f7cc768513a79bd
Robin Tram <robin.tram@gmail.com>
2018-10-19 02:08:21 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,6 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
     FROM series
     LEFT JOIN books
     ON series.id = books.series_id
+    WHERE 
     ORDER BY books.year
   ;"
 end
