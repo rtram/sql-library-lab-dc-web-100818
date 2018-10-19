a00ab645d396a37ee1ca45973e0ef7a3aee223b2
Robin Tram <robin.tram@gmail.com>
2018-10-19 01:29:36 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,4 +1,6 @@
-INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("A Song of Ice and Fire", 1, 1);
+
+INSERT INTO series (title, author_id, subgenre_id) VALUES ("Second Series", 2, 2);
 
 INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
 
