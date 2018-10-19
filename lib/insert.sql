f5443d193e27844982d2cf5795a4ab13997e1653
Robin Tram <robin.tram@gmail.com>
2018-10-19 01:45:22 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -25,6 +25,13 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Cha
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Character Three", "motto three", "cylon", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Character Four", "motto four", "cylon", 2, 2);
 
-INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 1); 
+INSERT INTO character_books (book_id, character_id) VALUES (1, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (2, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (3, 2);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 3);
+INSERT INTO character_books (book_id, character_id) VALUES (2, 3);
+INSERT INTO character_books (book_id, character_id) VALUES (3, 3);
+INSERT INTO character_books (book_id, character_id) VALUES (1, 4);
 
 -- -- INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
